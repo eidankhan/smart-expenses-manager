8cd80352f6d49166b22a0473006b38fecf46a720
eidancodez <eidankhan.student.muet@gmail.com>
2022-07-19 14:46:52 +0500

    Added a new end point to create a new document

--- src/main/java/com/expenses/manager/services/ExpenseService.java
+++ src/main/java/com/expenses/manager/services/ExpenseService.java
@@ -15,6 +15,7 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
 
 @Service
 public class ExpenseService implements ExpenseRepository {
@@ -23,8 +24,8 @@ public class ExpenseService implements ExpenseRepository {
     public GenericResponse findAll() {
         System.out.println("UserService.getAll() --> is called");
         GenericResponse response = new GenericResponse();
-        Firestore db = FirestoreInstance.getFirestoreInstance();
-        ApiFuture<QuerySnapshot> apiFuture = db.collection("expenses").get();
+        Firestore firestoreDB = FirestoreInstance.getFirestoreInstance();
+        ApiFuture<QuerySnapshot> apiFuture = firestoreDB.collection("expenses").get();
         List<Expense> expenses = new ArrayList<Expense>();
         try {
             List<QueryDocumentSnapshot> documents = apiFuture.get().getDocuments();
@@ -59,8 +60,26 @@ public class ExpenseService implements ExpenseRepository {
 
     @Override
     public GenericResponse save(Expense expense) {
-        // TODO Auto-generated method stub
-        return null;
+        System.out.println("UserService.getAll() --> is called");
+        GenericResponse response = new GenericResponse();
+        Firestore firestoreDB = FirestoreInstance.getFirestoreInstance();
+        ApiFuture<WriteResult> createdDocument = firestoreDB.collection("expenses").document(expense.getId().toString())
+                .create(expense);
+        try {
+            String documentUpdateTime = createdDocument.get().getUpdateTime().toString();
+            System.out.println("UserService.getAll() --> Document Updated At " + documentUpdateTime);
+            response.setCode(ResponseCode.SUCCESS.getCode());
+            response.setMessage(ResponseCode.SUCCESS.getMessage());
+            response.setData(expense);
+            return response;
+        } catch (InterruptedException | ExecutionException e) {
+            response.setCode(ResponseCode.INVALID_DATA.getCode());
+            response.setMessage(ResponseCode.INVALID_DATA.getMessage());
+            response.setData(e.getMessage());
+            System.out.println("UserService.getAll() --> Exception Occured");
+            e.printStackTrace();
+        }
+        return response;
     }
 
     @Override
