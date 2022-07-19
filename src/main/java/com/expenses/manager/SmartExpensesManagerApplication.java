d504cef35ca8003810e28db4e5235eaacd55bdaa
eidancodez <eidankhan.student.muet@gmail.com>
2022-07-19 12:16:31 +0500

    - Initial Commit

---
+++ src/main/java/com/expenses/manager/SmartExpensesManagerApplication.java
@@ -0,0 +1,14 @@
+package com.expenses.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpensesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpensesManagerApplication.class, args);
+		System.out.println("Spring Boot App is running");
+	}
+
+}
---
+++ src/test/java/com/expenses/manager/SmartExpensesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenses.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpensesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
